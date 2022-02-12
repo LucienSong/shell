64d912b08abfa329b107f342ff149dbca36d7657
Lucien Song <LucienSong@gmx.com>
2022-02-13 00:54:03 +0800

    Add i18n
    Add i18n and language selector

---
+++ assets/resources/index.js
@@ -0,0 +1,26 @@
+System.register("chunks:///_virtual/en.ts",["cc"],(function(e){"use strict";var t;return{setters:[function(e){t=e.cclegacy}],execute:function(){t._RF.push({},"09735e0vFxCtYp0kiZ9RQqC","en",void 0);var o=window,l=e("languages",{main:{labName:"Shell Crypto Laboratory",subtitle:"Connects Smart Contracts on the Blockchain",launch:"Launch",overviewInfo:"A new digital economic experiment based on the decentralized blockchain network that connects people all over the world",calculator:"",calculatorInfo:"Estimate your potential results from SHELL experiment by selecting the programs and the levels. The totals are indicated for 1 cycle of all selected levels.",calculatorInfo2:"* All calculations are for informational purposes only.",x3info:"A basic shell program that is best suited for those who are self-reliant and prefer to develop on their own.",x6info:"A more advanced program that is more team-oriented where the results can come from your direct partners or indirect as spillovers.",costOfAll:"Cost of all selected levels",totalResult:"Total results in 1 cycle from selected levels"}});o.languages||(o.languages={}),o.languages.en=l,t._RF.pop()}}}));
+
+System.register("chunks:///_virtual/zh_TW.ts",["cc"],(function(a){"use strict";var t;return{setters:[function(a){t=a.cclegacy}],execute:function(){t._RF.push({},"69ec73/dlxNNZHkVj099qjt","zh_TW",void 0);var e=window,c=a("languages",{main:{labName:"貝殼實驗室",subtitle:"為您連接區塊鏈上的智能合約",launch:"立即啟動",overviewInfo:"基於去中心化區塊鍊網絡的新型數字經濟實驗，連接世界各地的人們",calculator:"實驗收益計算器",calculatorInfo:"通過選擇項目和級別來估計您從實驗中獲得的潛在收益結果。總計顯示所有選定級別的一個循環週期。",calculatorInfo2:"* 所有計算僅供參考。",x3info:"一個基基礎的實驗項目，最適合那些自力更生並喜歡自己開發合作夥伴的人。",x6info:"一個更高級的計劃，更以團隊合作為導向，收益可以來自您的直接下級合作夥伴或間接獲得上級夥伴的溢出。",costOfAll:"所有選定級別的成本",totalResult:"選定級別的一個週期循環的總收益"}});e.languages||(e.languages={}),e.languages.zh_TW=c,t._RF.pop()}}}));
+
+System.register("chunks:///_virtual/jp.ts",["cc"],(function(e){"use strict";var a;return{setters:[function(e){a=e.cclegacy}],execute:function(){a._RF.push({},"fee7bvmOBVCzreX2eIum4ZZ","jp",void 0);var t=window,n=e("languages",{main:{labName:"シェルクリプトラボラトリー",subtitle:"ブロックチェーン上のスマートコントラクトを接続します",launch:"今すぐ参加",overviewInfo:"世界中の人々をつなぐ分散型ブロックチェーンネットワークに基づく新しいデジタル経済実験",calculator:"実験利益計算機",calculatorInfo:"プログラムとレベルを選択して、シェル実験からの潜在的な結果を推定します。 合計は、選択したすべてのレベルの1サイクルで示されます。",calculatorInfo2:"* すべての計算は情報提供のみを目的としています。",x3info:"自立していて自分で開発することを好む人に最適な基本的なシェルプログラム。",x6info:"結果が直接のパートナーから、または波及効果として間接的に得られる、よりチーム指向のより高度なプログラム。",costOfAll:"選択したすべてのレベルのコスト",totalResult:"選択したレベルからの1サイクルの合計結果"}});t.languages||(t.languages={}),t.languages.jp=n,a._RF.pop()}}}));
+
+System.register("chunks:///_virtual/resources",["./en.ts","./zh_TW.ts","./jp.ts"],(function(){"use strict";return{setters:[null,null,null],execute:function(){}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/resources', 'chunks:///_virtual/resources'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
