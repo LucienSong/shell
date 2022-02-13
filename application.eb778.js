2d1162e89b3e12f78a988a50c926939fb355c2be
Lucien Song <LucienSong@gmx.com>
2022-02-13 22:14:07 +0800

    Update calculator
    Update calculator

--- application.eb778.js
+++ application.eb778.js
@@ -96,7 +96,7 @@ System.register([], function (_export, _context) {
     }
 
     function loadSettingsJson(cc) {
-      var settings = 'src/settings.ec5a6.json';
+      var settings = 'src/settings.49764.json';
       return new Promise(function (resolve, reject) {
         if (typeof fsUtils !== 'undefined' && !settings.startsWith('http')) {
           var result = fsUtils.readJsonSync(settings);
--- index.9276d.js
+++ index.9276d.js
@@ -1,4 +1,4 @@
-System.register(["./application.0086b.js"], function (_export, _context) {
+System.register(["./application.eb778.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -67,8 +67,8 @@ System.register(["./application.0086b.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application0086bJs) {
-      createApplication = _application0086bJs.createApplication;
+    setters: [function (_applicationEb778Js) {
+      createApplication = _applicationEb778Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
