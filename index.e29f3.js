e999aa6107ce6236f60f52bc2e86d7a2dbecc6b4
Lucien Song <LucienSong@gmx.com>
2022-02-14 15:49:05 +0800

    Update register button in user scene
    Update register button in user scene

--- index.e29f3.js
+++ index.e29f3.js
@@ -1,4 +1,4 @@
-System.register(["./application.67000.js"], function (_export, _context) {
+System.register(["./application.5ecc2.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -67,8 +67,8 @@ System.register(["./application.67000.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application67000Js) {
-      createApplication = _application67000Js.createApplication;
+    setters: [function (_application5ecc2Js) {
+      createApplication = _application5ecc2Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
