87b50bb1174185e22ec1b4c51c18a9d7b8225f84
Lucien Song <LucienSong@gmx.com>
2022-02-16 01:40:18 +0800

    Add newTech
    Add newTech

--- index.6d345.js
+++ index.6d345.js
@@ -1,4 +1,4 @@
-System.register(["./application.4ce13.js"], function (_export, _context) {
+System.register(["./application.951f7.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -67,8 +67,8 @@ System.register(["./application.4ce13.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application4ce13Js) {
-      createApplication = _application4ce13Js.createApplication;
+    setters: [function (_application951f7Js) {
+      createApplication = _application951f7Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
