6c4fd1be165eed6b050514b25ef9727773fa67b6
Lucien Song <LucienSong@gmx.com>
2022-02-16 20:17:30 +0800

    Update FAQ

--- index.22c24.js
+++ index.22c24.js
@@ -1,4 +1,4 @@
-System.register(["./application.c2731.js"], function (_export, _context) {
+System.register(["./application.868b4.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -67,8 +67,8 @@ System.register(["./application.c2731.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationC2731Js) {
-      createApplication = _applicationC2731Js.createApplication;
+    setters: [function (_application868b4Js) {
+      createApplication = _application868b4Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
