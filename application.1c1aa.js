97c8bb4891cd5096f3e996a9699499f2f9df9943
Lucien Song <LucienSong@gmx.com>
2022-02-17 21:50:19 +0800

    Update infoNode.prefab
    Update infoNode.prefab

--- application.1c1aa.js
+++ application.1c1aa.js
@@ -96,7 +96,7 @@ System.register([], function (_export, _context) {
     }
 
     function loadSettingsJson(cc) {
-      var settings = 'src/settings.29c0d.json';
+      var settings = 'src/settings.4c7f9.json';
       return new Promise(function (resolve, reject) {
         if (typeof fsUtils !== 'undefined' && !settings.startsWith('http')) {
           var result = fsUtils.readJsonSync(settings);
--- index.91ccc.js
+++ index.91ccc.js
@@ -1,4 +1,4 @@
-System.register(["./application.ef02b.js"], function (_export, _context) {
+System.register(["./application.1c1aa.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -67,8 +67,8 @@ System.register(["./application.ef02b.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationEf02bJs) {
-      createApplication = _applicationEf02bJs.createApplication;
+    setters: [function (_application1c1aaJs) {
+      createApplication = _application1c1aaJs.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
