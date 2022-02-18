557a5c140856dc31e0271e5a664062db500d4358
Lucien Song <LucienSong@gmx.com>
2022-02-18 10:45:34 +0800

    Update

--- index.9c0d6.js
+++ index.9c0d6.js
@@ -1,4 +1,4 @@
-System.register(["./application.af8e2.js"], function (_export, _context) {
+System.register(["./application.dd469.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -67,8 +67,8 @@ System.register(["./application.af8e2.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationAf8e2Js) {
-      createApplication = _applicationAf8e2Js.createApplication;
+    setters: [function (_applicationDd469Js) {
+      createApplication = _applicationDd469Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
