50782683891c094b31932d380d959099db1d0bb7
Lucien Song <LucienSong@gmx.com>
2022-02-20 14:30:02 +0800

    Update copy button

--- index.e630f.js
+++ index.e630f.js
@@ -1,4 +1,4 @@
-System.register(["./application.989aa.js"], function (_export, _context) {
+System.register(["./application.797c0.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -67,8 +67,8 @@ System.register(["./application.989aa.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application989aaJs) {
-      createApplication = _application989aaJs.createApplication;
+    setters: [function (_application797c0Js) {
+      createApplication = _application797c0Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
