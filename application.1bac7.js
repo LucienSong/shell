575d8fbaa1b637a04db2ea6b89ef787dedacf04e
Lucien Song <LucienSong@gmx.com>
2022-02-27 01:03:10 +0800

    add count down

--- application.1bac7.js
+++ application.1bac7.js
@@ -96,7 +96,7 @@ System.register([], function (_export, _context) {
     }
 
     function loadSettingsJson(cc) {
-      var settings = 'src/settings.f378c.json';
+      var settings = 'src/settings.b9513.json';
       return new Promise(function (resolve, reject) {
         if (typeof fsUtils !== 'undefined' && !settings.startsWith('http')) {
           var result = fsUtils.readJsonSync(settings);
--- index.967ce.js
+++ index.967ce.js
@@ -1,4 +1,4 @@
-System.register(["./application.eb852.js"], function (_export, _context) {
+System.register(["./application.1bac7.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -67,8 +67,8 @@ System.register(["./application.eb852.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationEb852Js) {
-      createApplication = _applicationEb852Js.createApplication;
+    setters: [function (_application1bac7Js) {
+      createApplication = _application1bac7Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
