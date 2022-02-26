5becefdb243bc2ec6b0c0f585f8394d7e66ebced
Lucien Song <LucienSong@gmx.com>
2022-02-27 00:20:50 +0800

    Update

--- application.eb852.js
+++ application.eb852.js
@@ -96,7 +96,7 @@ System.register([], function (_export, _context) {
     }
 
     function loadSettingsJson(cc) {
-      var settings = 'src/settings.c7905.json';
+      var settings = 'src/settings.f378c.json';
       return new Promise(function (resolve, reject) {
         if (typeof fsUtils !== 'undefined' && !settings.startsWith('http')) {
           var result = fsUtils.readJsonSync(settings);
--- index.285d3.js
+++ index.285d3.js
@@ -1,4 +1,4 @@
-System.register(["./application.adda1.js"], function (_export, _context) {
+System.register(["./application.eb852.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -67,8 +67,8 @@ System.register(["./application.adda1.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationAdda1Js) {
-      createApplication = _applicationAdda1Js.createApplication;
+    setters: [function (_applicationEb852Js) {
+      createApplication = _applicationEb852Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
