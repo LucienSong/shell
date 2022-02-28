495aee9c25c6dee9de0c5e13cf1dc1ff7c0f67bb
Lucien Song <LucienSong@gmx.com>
2022-02-28 17:10:38 +0800

    Update

--- index.896f5.js
+++ index.896f5.js
@@ -1,4 +1,4 @@
-System.register(["./application.60e81.js"], function (_export, _context) {
+System.register(["./application.2c581.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -67,8 +67,8 @@ System.register(["./application.60e81.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application60e81Js) {
-      createApplication = _application60e81Js.createApplication;
+    setters: [function (_application2c581Js) {
+      createApplication = _application2c581Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
