c2808e17ed886996d861e5bb87f2ec2a0ee695e1
Lucien Song <LucienSong@gmx.com>
2022-02-28 09:33:00 +0800

    Update checkIn
    Update checkIn

--- index.ffd4b.js
+++ index.ffd4b.js
@@ -1,4 +1,4 @@
-System.register(["./application.ee7ae.js"], function (_export, _context) {
+System.register(["./application.e97b9.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -67,8 +67,8 @@ System.register(["./application.ee7ae.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationEe7aeJs) {
-      createApplication = _applicationEe7aeJs.createApplication;
+    setters: [function (_applicationE97b9Js) {
+      createApplication = _applicationE97b9Js.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
